227e17df9a02926257554b0ba7ae57db4543e80d
Joshua <joshuaarnaez22@gmail.com>
2024-03-10 14:07:50 +0800

    ...

--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -17,7 +17,6 @@ const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
     const io = new ServerIO(httpServer, {
       path: path,
       addTrailingSlash: false,
-      transports: ["websocket", "polling"],
     });
     res.socket.server.io = io;
   }
